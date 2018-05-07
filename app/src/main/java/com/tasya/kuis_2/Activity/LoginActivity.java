969cb276f2ee27f791d2907aca5c573df4a402ed
tasyaltm <tazkiyahtasya@gmail.com>
2018-05-07 16:25:19 +0700

    add

---
+++ app/src/main/java/com/tasya/kuis_2/Activity/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.tasya.kuis_2.Activity;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import com.tasya.kuis_2.API.ApiClient;
+import com.tasya.kuis_2.API.ApiInterface;
+import com.tasya.kuis_2.API.ApiData;
+import com.tasya.kuis_2.Model.Users;
+import com.tasya.kuis_2.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
