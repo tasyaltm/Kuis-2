59a4875b3f23649a784169620c2bc9ba11b96468
tasyaltm <tazkiyahtasya@gmail.com>
2018-05-07 16:38:57 +0700

    add

---
+++ app/src/main/java/com/tasya/kuis_2/Activity/TransferActivity.java
@@ -0,0 +1,15 @@
+package com.tasya.kuis_2.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tasya.kuis_2.R;
+
+public class TransferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+    }
+}
